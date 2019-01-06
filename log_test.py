ba140b05e701e8d4d1ffb0f9b64d6e87512a0ea8
ksolowoniuk <ksolowoniuk@hotmail.com>
2019-01-06 13:33:17 -0700

    --first use of plotly.

--- log_test.py
+++ log_test.py
@@ -1,4 +1,16 @@
 import math
+import plotly
+import plotly.graph_objs as go 
 
+yPlt = []
+xPlt = []
 for i, j in [(x, math.log10(x)) for x in range(1,101)]:
-    print(i, j)
+    yPlt.append(j)
+    xPlt.append(i)
+
+plotly.offline.plot(
+{
+    "data": [go.Scatter(x=xPlt, y=yPlt)],
+    "layout": go.Layout(title="Plotly log() Test")
+}, auto_open=True
+)
