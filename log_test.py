27863fe92aca0caf861f94c81282d435954d38a9
ksolowoniuk <ksolowoniuk@hotmail.com>
2019-01-06 15:07:10 -0700

    --general cleanup.

--- log_test.py
+++ log_test.py
@@ -4,9 +4,11 @@ import plotly.graph_objs as go
 
 yPlt = []
 xPlt = []
+
 for i, j in [(x, math.log10(x)) for x in range(1,1001)]:
     yPlt.append(j)
     xPlt.append(i)
+    print(i, j)
 
 plotly.offline.plot(
 {
